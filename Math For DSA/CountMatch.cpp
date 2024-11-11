3f5f31856e0016b7df7fbab30e1c242d32287592
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-11-11 13:07:05 +0530

    leetCode - count-of-matches-in-tournament

---
+++ Math For DSA/CountMatch.cpp
@@ -0,0 +1,8 @@
+// leetcode - https://leetcode.com/problems/count-of-matches-in-tournament/
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans = n-1;
+        return ans;
+    }
+};
