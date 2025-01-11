575d6b8d5b28f2f4da0cb48a4b1b0ca27269984b
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-11 10:21:02 +0530

    Same Tree in BT

---
+++ Tree/SameTree.cpp
@@ -0,0 +1,18 @@
+// link ->https://leetcode.com/problems/same-tree/
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL) return true;
+        if(p==NULL || q==NULL) return false;
+        if(p->val == q->val){
+            bool left = isSameTree(p->left, q->left);
+            bool right = isSameTree(p->right, q->right);
+            return(left && right);
+        }
+        else{
+            return false;
+        }
+
+        
+    }
+};
