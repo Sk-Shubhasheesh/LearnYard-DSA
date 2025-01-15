6f511165d1a24289acd15d6887d357ae63108133
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-15 11:11:06 +0530

    symmetric-tree in BT

---
+++ Tree/SymmetricTree.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/symmetric-tree/
+class Solution {
+public:
+    bool isSymmetricHelper(TreeNode* leftNode, TreeNode* rightNode){
+        if(leftNode==NULL && rightNode==NULL) return true;
+        if(leftNode==NULL || rightNode==NULL || leftNode->val != rightNode->val) return false;
+        
+        return isSymmetricHelper(leftNode->left, rightNode->right) && isSymmetricHelper(leftNode->right, rightNode->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return isSymmetricHelper(root->left, root->right);
+    }
+};
